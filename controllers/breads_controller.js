8ba43e454a872c839e1a0de8313c8225a5289e1a
yournewgoodname <yournew@email.com>
2023-11-19 10:26:39 -0800

    adding data, js files

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const breads = express.Router()
+const Bread = require('../models/bread.js')
 
+// http://localhost:3000/breads/
 breads.get('/', (req, res) =>{
     res.send('This is an index of breads')
+})
+
+// show
+breads.get('/:arrayIndex', (req, res) =>{
+    res.send(Bread[req.params.arrayIndex])
 })
 
 module.exports = breads
